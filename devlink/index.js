4698f6e8d80eb83922804a55072af7055c638fe8
OVIS <ovi@ovswebsites.com>
2024-01-10 18:23:38 +0200

    feat: added webflow devlink connection, imported sample Relume components

---
+++ devlink/index.js
@@ -0,0 +1,12 @@
+"use client";
+
+export * as _Builtin from "./_Builtin";
+export * from "./devlinkContext";
+export * from "./interactions";
+export * from "./utils";
+export * from "./devlink";
+export * from "./Feature1";
+export * from "./Feature192";
+export * from "./GlobalStyles";
+export * from "./Hero1";
+export * from "./Hero5";
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "uploads-ssl.webflow.com",
+      "images.unsplash.com",
+      "images.prismic.io",
+    ],
+  },
 };
 
 module.exports = nextConfig;
