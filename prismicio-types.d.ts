69fe0185aadfdb2e47e932bb7ea5a2171c8af33a
OVIS <ovi@ovswebsites.com>
2024-01-12 09:22:25 +0200

    this might be a mistake - added slices from old project again

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -4,7 +4,10 @@ import type * as prismic from "@prismicio/client";
 
 type Simplify<T> = { [KeyType in keyof T]: T[KeyType] };
 
-type PageDocumentDataSlicesSlice = RichTextSlice;
+type PageDocumentDataSlicesSlice =
+  | FeatureSlice
+  | HeroHeadingSlice
+  | RichTextSlice;
 
 /**
  * Content for Page documents
